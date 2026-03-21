d8bc9e078ab1857ac5e45e0ad8a25c967226ddd7
Silletr <hristicenkoigor764@gmail.com>
2026-03-21 11:10:09 +0200

    [CHANGED FILE/DIR: README.md, pyproject.toml | NEW FILE/DIR: .gitignore, poetry.lock | MOVED FILE/DIR: src/auto-ahk-switch/* -> src/ahk-switch] Well, src/ahk-switch need for Poetry (To install all depencies correctly it asked me to rename - Who am I to decline this offer?). Also added finding .ahk  scripts (For now - yeah, .ahk, but then i'll switch to Linux analogue. Just AHK is the first my thought) with printing all file inside (carefully with that, if file big it may stuck or just make mess in your terminal) [TIMESTAMP: 21/03/2026 | 11:10]

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Auto-AHK-Switch
-Switcher for your AHK scripts! Supports Windows GUI and Linux TUI/CLI!
+# 🔀 Auto-AHK-Switch
+
+**What is using**:
+| Tool | Why |
+| -------------- | --------------- |
+| loguru  | For logging everywhat (i dont know what exactly) |
+| xdotool, xbindkeys | Like a UNIX analogue to AHK (Sorry, didn't found native AHK port) |
---
+++ src/ahk-switch/find_ahk.py
@@ -0,0 +1,46 @@
+import os
+
+from subprocess import (
+    CalledProcessError,
+    CompletedProcess,
+    run,
+)  # , CalledProcessError WILL BE USED IN FUTURE
+
+# from pathlib import Path  # Optional: Need for cleaner paths
+from loguru import logger
+
+
+# === Find AHK scripts on the most cummon places ===
+def find_ahk(name: str, path: str) -> str:
+    full_path = os.path.expanduser(path)  # Expand ~ to /home/UserName
+    logger.info(f"Searching from: {full_path}")
+
+    for root, dirs, files in os.walk(top=full_path):
+        if name in files:
+            found_path: str = os.path.join(root, name)
+            logger.success(f"File found: {found_path}!")
+            try:
+                file_inside: CompletedProcess[str] = run(
+                    args=["cat", found_path], capture_output=True, text=True, check=True
+                )
+
+                if not file_inside.stdout.strip():  # Properly check empty/whitespace
+                    logger.warning(f"File on {found_path} is empty!")
+                    return "File is empty"
+
+                # logger.success(file_inside.stdout)
+                return f"File Path: {found_path} | Script inside:\n{file_inside.stdout}"
+
+            except CalledProcessError as error:
+                logger.error(f"STDERR: {error.stderr}")
+                return f"Error reading {found_path}"
+
+    raise FileNotFoundError(f"Script '{name}' not found starting from {full_path}!")
+
+
+def main() -> None:
+    result: str = find_ahk(name="test.ahk", path="~/")
+    logger.info(result)
+
+
+main()
