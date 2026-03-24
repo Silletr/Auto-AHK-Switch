15536b5d6971c074b62cc3efc4c23e87f93e78be
Silletr <hristicenkoigor764@gmail.com>
2026-03-24 11:41:36 +0200

    [CHANGED FILE/DIR: poetry.lock, pyproject.toml, src/ahk-switch/main.py, src/ahk-switch/switch_ahk.py] First of all - switch_ahk is simple as log. Second one - I decided no to do a Linux version, cuz its too fucking hard, and I think better to make it as anther one project, but later. Also, tried to make it work from Windows 11 CMD but have no output when pressing hotkey. Will be fixed later [TIMESTAMP: 24/03/2026 | 11:41 UTC+2]

--- src/ahk-switch/main.py
+++ src/ahk-switch/main.py
@@ -1,27 +1,31 @@
-from pynput import keyboard
+import keyboard
+from loguru import logger
 from find_ahk import find_ahk
 from switch_ahk import switch
 
 
 def main() -> None:
-    scripts = find_ahk()
-
+    scripts: list[str] = find_ahk(extension=".py")
     if not scripts:
         print("No scripts found!")
         return
 
-    # build hotkey map
-    hotkeys: dict[str, any] = {}  # pyright: ignore
-    for i, script_path in enumerate(scripts, 1):
-        if i > 9:
-            break
-        hotkeys[f"<alt>+{i}"] = lambda p=script_path: switch(to=p)
-        print(f"Alt+{i} → {script_path}")
+    script_map: dict[str, str] = {}
+    for index in range(min(9, len(scripts))):
+        script_map[str(index + 1)] = scripts[index]
 
-    print("Listening... Ctrl+C to exit")
+    for key_num_str, script_path in script_map.items():
+        print(f"Alt+{key_num_str} → {script_path}")
 
-    with keyboard.GlobalHotKeys(hotkeys) as h:
-        h.join()
+    print("Listening... Press Ctrl+C to exit")
+
+    for key_num_str, script_path in script_map.items():
+        keyboard.add_hotkey(
+            f"alt+{key_num_str}", lambda p=script_path: switch(p), suppress=True
+        )
+
+    logger.success("Hook installed!")
+    keyboard.wait()
 
 
 if __name__ == "__main__":
--- src/ahk-switch/switch_ahk.py
+++ src/ahk-switch/switch_ahk.py
@@ -1,28 +1,7 @@
-from subprocess import Popen
-import os
+import subprocess
 from loguru import logger
 
-current_process: Popen | None = None  # type: ignore
 
-
-def switch(to: str) -> None:
-    global current_process
-    cmd: list[str] = [""]
-    # Check file exists
-    if not os.path.exists(path=to):
-        logger.critical(f"Script {to} not found")
-        return
-
-    if to.endswith(".py"):
-        cmd: list[str] = ["python", to]
-    elif to.endswith(".ahk"):
-        cmd: list[str] = ["AutoHotKey.exe", to]
-    elif to.endswith(".sh"):
-        cmd: list[str] = ["./", to]
-
-    else:
-        logger.error(f"Unkown file extension: {to}")
-        return
-
-    current_process = Popen(cmd)
-    logger.success(f"Switched to: {os.path.basename(to)}")
+def switch(path: str) -> None:
+    logger.info(f"Switching to: {path}")
+    subprocess.Popen(["python", path])
